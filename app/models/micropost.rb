dd04dd331f58a7df41e22440f1ff358487757781
FUMIGear <iso.fumi429@gmail.com>
2024-03-23 15:43:43 +0900

    第２章のまとめ手前まで進めました。

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
-  
+  belongs_to :user # リスト2.12
+  validates :content, length: { maximum: 140 }, presence: true # リスト2.11と演習2
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts # リスト2.11
+  validates :name, presence: true # リスト2.11と演習2
+  validates :emailz, presence: true # リスト2.11と演習2
 end
-
