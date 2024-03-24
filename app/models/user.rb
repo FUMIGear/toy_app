a5d4c7eee694e7ebc9f57bd199fb4e980acd293a
FUMIGear <iso.fumi429@gmail.com>
2024-03-25 07:40:48 +0900

    Renderで動作確認したら、エラーが発生した。誤字があったので修正しました。 showページは一旦、最初のポストは表示しないようにしました（エラーが出たため）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts # リスト2.11
   validates :name, presence: true # リスト2.11と演習2
-  validates :emailz, presence: true # リスト2.11と演習2
+  validates :email, presence: true # リスト2.11と演習2
 end
